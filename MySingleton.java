11e03ac756b1a925e26ec2a686f6161fb5cf6759
thailypost <thaily.chea@gmail.com>
2017-11-14 21:25:40 +0700

    Class MySingleton.java

---
+++ MySingleton.java
@@ -0,0 +1,36 @@
+package com.thaily.freshnews;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by CHEA THAILY on 11/8/2017.
+ */
+
+public class MySingleton {
+    private static MySingleton mInstance;
+    private RequestQueue requestQueue;
+    private static Context context;
+    private MySingleton(Context context){
+        context=context;
+        requestQueue=getRequestQueue();
+    }
+    public RequestQueue getRequestQueue(){
+        if(requestQueue==null){
+            requestQueue= Volley.newRequestQueue(context.getApplicationContext());
+        }
+        return requestQueue;
+    }
+    public static synchronized MySingleton getInstance(Context context){
+        if(mInstance==null){
+            mInstance=new MySingleton(context);
+        }
+        return mInstance;
+    }
+    public <T>void addToRequestque(Request<T> request){
+        requestQueue.add(request);
+    }
+}
